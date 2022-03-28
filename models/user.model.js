19c93c50806573acdeb6f04b3cf3fde8b859145e
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-03-28 17:13:55 +0200

    update / delete des profils user + salade des mots de passe

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
     {
@@ -24,6 +25,10 @@ const userSchema = new mongoose.Schema(
             max: 1024,
             minLength: 8
         },
+        picture: {
+            type: String,
+            default: "/uploads/profil/random-user.png"
+        },
         bio: {
             type: String,
             max: 1024
@@ -41,7 +46,14 @@ const userSchema = new mongoose.Schema(
     {
         timestamps: true,
     }
-)
+);
+
+// fonction de salage qui se jouera avant la sauvegarde des données
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 const UserModel = mongoose.model('user', userSchema);
 
