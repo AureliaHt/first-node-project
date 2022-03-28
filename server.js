5111f5fe16d6ab65414d7a46377e60ad122b4b17
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-03-28 14:55:43 +0200

    création de la table users et test sur  Postman

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use('/api/user', userRoutes);
+
+// server 
 app.listen(5000, () => {
     console.log(`listenning on port ${process.env.PORT}`);
 })
